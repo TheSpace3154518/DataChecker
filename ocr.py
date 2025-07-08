74935616bf18fd7df5d04255297d3314b352c9a1
SpaceAttay315 <MohamedAmine.Kalai315@gmail.com>
2025-07-08 10:06:19 +0100

    OCR Working

--- ocr.py
+++ ocr.py
@@ -5,11 +5,42 @@ from PIL import Image, ImageEnhance, ImageFilter, ImageFont, ImageDraw
 import numpy as np
 import cv2
 
-# ======================= Variables ======================= #
-ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+# ======================= TODO ======================= #
+# Check Doc
+# Crop
+# Refactoring
 
 
 
+# ======================= Constants ======================= #
+ALLOWED_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '"
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+# ======================= Debugging ======================= #
+def draw_bbox(img, bbox):
+    image = Image.open(img)
+    draw = ImageDraw.Draw(image)
+    box = (bbox[0][0], bbox[0][1], bbox[2][0], bbox[2][1])
+    draw.rectangle(box, outline='red', width=2)
+    image.show()
+
+def check_ocr():
+    mistakes = {}
+
+    for letter in ALPHABET:
+        print("- "*30 + " Processing Letter: " + letter + " -"*30)
+        results = process_image("./cins/", f'cin_{letter}.png')
+        print(results)
+        if (results[0][0] != letter):
+            mistakes[letter] = results[0][0]
+
+    return mistakes
+
+
+
+
+# ======================= Main Program ======================= #
 def preprocess_image(img):
 
     # Load the image
@@ -41,28 +72,11 @@ def preprocess_image(img):
 
 def read_text_from_image(image, val):
 
-    # img = Image.open(image)
-    # config = r'--oem 3 --psm ' + str(val)
-    # results = pytesseract.image_to_string(img, lang='eng+fr', config=config)
-
     reader = easyocr.Reader(['fr', 'en'], gpu=True)
-    results = reader.readtext(image, detail=1, allowlist=ALLOWED_CHARS)
+    results = reader.readtext(image, allowlist=ALLOWED_CHARS)
 
     return results
 
-
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-def check_ocr():
-    mistakes = {}
-
-    for letter in alphabet:
-        print("- "*30 + " Processing Letter: " + letter + " -"*30)
-        results = process_image("./cins/", f'cin_{letter}.png')
-        print(results)
-        if (results[0][0] != letter):
-            mistakes[letter] = results[0][0]
-
-    return mistakes
 
 def draw_letter(text, folder, img, bbox):
     image = Image.open(folder + img)
@@ -79,18 +93,6 @@ def draw_letter(text, folder, img, bbox):
     image.save(folder + "corrected_" + img)
 
 
-
-def draw_bbox(img, bbox):
-
-    image = Image.open(img)
-    draw = ImageDraw.Draw(image)
-
-    # Define box (left, top, right, bottom)
-    box = (bbox[0][0], bbox[0][1], bbox[2][0], bbox[2][1])
-    draw.rectangle(box, outline='red', width=2)
-    image.show()
-
-
 def process_image(folder, path, correcting=False):
     if correcting:
         # Preprocessing
@@ -98,40 +100,48 @@ def process_image(folder, path, correcting=False):
         processed_image = Image.fromarray(processed_image)
         processed_image.save(folder + "processed_" + path, dpi=(300, 300))
 
-        # Perform OCR
+        # Correct Image by adding letter
         data = read_text_from_image(folder + "processed_" + path, 6)
-
         for res in data :
             # draw_bbox(folder + path, res[0])
             draw_letter("A", folder, "processed_" + path, res[0])
 
+        text = [result[1] for result in data]
+
     if not correcting:
-        # processed_image = preprocess_image(folder + path)
-        # processed_image = Image.fromarray(processed_image)
-        # processed_image.save(folder + "processed_" + path, dpi=(300, 300))
 
         # Perform OCR
         data = read_text_from_image(folder + "corrected_processed_" + path, 6)
 
-    text = [f"{result[1]} | conf : {result[2]:.2f}" for result in data]
+        text = [result[1] for result in data]
+        for i, result in enumerate(text):
+            result = result[1:]
+            if result[0] == '0' :
+                result = "O" + result[1:]
+            elif result[1] == '0' and result[0] == 'D':
+                result = "undefined"
+            text[i] = result
+
+
     return text
 
+
+# ======================= Test Program ======================= #
 if __name__ == "__main__":
 
     imgs = ["cropped-2.png", "test_cin2.png"]
-    # imgs = [f'cin_{letter}.png' for letter in alphabet]
+    # imgs = [f'cin_{letter}.png' for letter in ALPHABET]
     # imgs = [f'test{i}.png' for i in range(1,9)]
     # imgs = ["cin2.png", "cin-1.png"]
 
     folder ="./Recruits/"
 
-    # Take 1
     for img in imgs:
 
         # Add Correction
         print("-"*30 + " Correcting Image: " + img + "-"*30)
         results = process_image(folder, img, correcting=True)
-        print("\n".join(results))
+        print(f"Saved at {folder + 'corrected_processed_' + img}")
 
         # Attempt to read
         print("-"*30 + " Processing Image: " + img + "-"*30)
